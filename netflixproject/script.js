d6dfdb04fbfa3321988bc08749f379290137a4e7
yogesh-dev10256 <137671150+yogesh-dev10256@users.noreply.github.com>
2023-07-01 12:54:01 +0530

    Add files via upload

---
+++ netflixproject/script.js
@@ -0,0 +1,64 @@
+let navbar=document.getElementById("navbar");
+
+const toggel=()=>{
+if(navbar.style.display == "none"){
+    navbar.style.display="block";
+    navbar.style.textAlign="left";
+}
+else{
+    navbar.style.display="none";
+}
+}
+
+function carousel(){
+    let carouselslider = document.querySelector(".carousel-slider");
+    let list=document.querySelector(".carousel-list");
+    let item=document.querySelector(".carousel-item");
+    let list2;
+     
+    const speed =1;
+
+    const width=list.offsetWidth;
+    let x=0;
+    let x2= width;
+
+function clone(){
+    list2=list.cloneNode(true);
+    carouselslider.appendChild(list2);
+    list2.style.left=`${width}px`;
+
+}
+function moveFirst(){
+    x-=speed;
+    if(width>=Math.abs(x)){;
+        list2.style.left=`${x}px`;
+    }else{
+        x=width;
+    }
+}
+function moveSecond(){
+    x2-=speed;
+    if(list.offsetWidth>= Math.abs(x2)){
+        list2.style.left=`${x2}px`;
+
+    }else{
+        x2=width;
+    }
+}
+function hover(){
+    clearInterval(a);
+    clearInterval(b);
+    
+}
+function unhover(){
+    a = setInterval(moveFirst,10);
+    b = setInterval(moveSecond,10);
+}
+
+clone()
+    let a = setInterval(moveFirst,10);
+    let b = setInterval(moveSecond,10);
+    carouselslider.addEventListener("mouseenter",hover);
+    carouselslider.addEventListener("mouseleave",unhover);
+}
+carousel()
